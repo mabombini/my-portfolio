a94d6704e5ad1a60dd96aa26a41d0d38d72ed275
Marina Bombini <bombinimarina@gmail.com>
2026-08-06 22:39:34 +1000

    First commit

---
+++ marina-porfolio/src/components/TopBar.jsx
@@ -0,0 +1,17 @@
+function TopBar(){
+    return(
+        <nav>
+
+    <div class="logo">
+        <strong>MARINA BOMBINI</strong> / Full Stack Developer
+    </div>
+
+    <div class="links">
+        <a href="">LINKEDIN</a>
+        <a href="">GITHUB</a>
+        <a href="">CONTACT ME</a>
+    </div>
+
+</nav>
+    )
+}
